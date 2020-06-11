eff3a20f738ae9a60bc1004e1935b94b864353e5
Ronaldo Francelino Portela <ronaldoportela@gmail.com>
2020-06-11 18:54:26 -0300

    form de cadastro de produto, upload file

--- QuickBuy2.Web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuy2.Web/ClientApp/src/app/produto/produto.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProdutoServico } from '../servicos/produto/produto.servico';
+import { Produto } from '../model/produto';
 
 @Component({
     selector: 'app-produto',
-    templateUrl: './produto.component.html'
+    templateUrl: './produto.component.html',
+    styleUrls:['./produto.component.css']
 })
 
-export class ProdutoComponent {
+export class ProdutoComponent implements OnInit {
 
+    public produto: Produto;
 
-    public obterNome(): string {
-        return "Produto-Component funcionando";
+    constructor(private produtoServico: ProdutoServico) {
+
     }
+
+    ngOnInit(): void {
+        this.produto = new Produto();
+    }
+
+    public Cadastrar() {
+        this.produtoServico.cadastrar(this.produto)
+            .subscribe(
+                produtoJson => {
+                    console.log(produtoJson);
+                },
+                e => {
+                    console.log(e.error);
+                }
+            );
+    }
+    
 }
---
+++ QuickBuy2.Web/ClientApp/src/app/servicos/produto/produto.servico.ts
@@ -0,0 +1,58 @@
+import { Injectable, Inject, OnInit } from "@angular/core";
+import { HttpClient, HttpHandler, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Produto } from "../../model/produto";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ProdutoServico implements OnInit {
+    
+
+    private _baseUrl: string;
+    public produtos: Produto[];
+
+    get header(): HttpHeaders {
+        return new HttpHeaders().set('content-type', 'application/json');
+    }
+
+    constructor(
+        private http: HttpClient,
+        @Inject('BASE_URL') baseUrl: string
+    ) {
+        this._baseUrl = baseUrl;
+    }
+
+    ngOnInit(): void {
+        this.produtos = [];
+    }
+
+
+    public cadastrar(produto: Produto): Observable<Produto> {
+
+        return this.http.post<Produto>(this._baseUrl + 'api/produto/cadastrar', JSON.stringify(produto), { headers: this.header });
+    }
+
+    public salvar(produto: Produto): Observable<Produto> {
+
+        return this.http.post<Produto>(this._baseUrl + 'api/produto/salvar', JSON.stringify(produto), { headers: this.header });
+    }
+
+    public deletar(produto: Produto): Observable<Produto> {
+
+        return this.http.post<Produto>(this._baseUrl + 'api/produto/deletar', JSON.stringify(produto), { headers: this.header });
+    }
+
+    public obterTodos(): Observable<Produto[]> {
+
+        return this.http.get<Produto[]>(this._baseUrl + 'api/produto/');
+    }
+
+    public obterProduto(produtoId: number): Observable<Produto> {
+
+        return this.http.get<Produto>(this._baseUrl + 'api/produto/');
+    }
+
+}
+
