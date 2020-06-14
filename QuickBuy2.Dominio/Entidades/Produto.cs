bd4d7f31f9843cbbeb5304bb97f9400ea4d05dd9
Ronaldo Francelino Portela <ronaldoportela@gmail.com>
2020-06-14 10:52:45 -0300

    - completar validação da tela de cadastro do produto; - inclusão /execução da migration para a coluna  NomeArquivo na tabela de produto;

--- QuickBuy2.Dominio/Entidades/Produto.cs
+++ QuickBuy2.Dominio/Entidades/Produto.cs
@@ -10,6 +10,7 @@ namespace QuickBuy2.Dominio.Entidades
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
+        public string NomeArquivo { get; set; }
 
         public override void Validate()
         {
